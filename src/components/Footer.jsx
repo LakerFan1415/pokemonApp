a836aa4467b3fb586016db9667c2ad8e8739eb23
LakerFan1415 <martybj32Gmail.com>
2022-04-05 19:49:08 -0400

    Updated Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,7 @@
 const Footer = () => {
     return (
         <footer>
-            <p>This application is powered by the <a className="footer-link" href="https://pokeapi.co/docs/v2" target="_blank">PokeApi</a></p>
+            <p>This application is powered by the <a className="footer-link" href="https://pokeapi.co/docs/v2" target="_blank" rel="noreferrer" >PokeApi</a></p>
         </footer>
     )
 }
